c913e4411549a314c285b509b01bc4476b67107e
Tomas Sykora, jr <jump@syky.cz>
2016-10-26 19:34:10 +0200

    CODE: Initial Commit

---
+++ LostSocks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LostSocks
+//
+//  Created by Tomas Sykora, jr. on 26/10/2016.
+//  Copyright © 2016 AJTY, s.r.o. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
